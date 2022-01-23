0890fdf74e64f512f74969a54ff80a22a747c048
scriptvpskita <88125458+scriptvpskita@users.noreply.github.com>
2022-01-24 02:17:45 +0800

    Create ssh-ws-ssl.sh

---
+++ ssh-ws-ssl.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+##autoscript_install_sshws+ssl443
+#Author : william
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/xkjdox/rijrekeksdpdolwoqkqkakodix/main/Nxbdjekkwso.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+
+# step one : install package
+apt install nodejs
+apt install npm
+cd /etc/william/
+wget -O PDirect.js "https://raw.githubusercontent.com/xkjdox/sojsiws/main/ndjdjdjdi.js"
+chmod +x PDirect.js
+
+# step two : configurasi sshws+ssl443
+cat > /etc/systemd/system/cdn.service << END 
+[Unit]
+Description=P7COM-nodews1-WILLIAM
+Documentation=https://p7com.net/
+Documentation=https://t.me/user_legend
+After=network.target nss-lookup.target
+
+[Service]
+User=nobody
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=node --expose-gc /etc/william/PDirect.js -dhost 127.0.0.1 -dport 109 -mport 2052
+Restart=on-failure
+RestartSec=3s
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# step three : enable service and reboot
+systemctl enable cdnjs.service
+systemctl start cdnjs.service
+clear
+echo "INSTALL COMPLETED ! AUTOREBOOT ON 3 SEC."
+sleep 1
+echo "1"
+sleep 1
+echo "2"
+sleep
+echo "3"
+sleep 1
+reboot
