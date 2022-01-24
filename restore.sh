b5affca7306c8dbe9c1e4b17d77c9c0e8ae9e698
scriptvpskita <88125458+scriptvpskita@users.noreply.github.com>
2022-01-24 13:21:41 +0800

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/xkjdox/rijrekeksdpdolwoqkqkakodix/main/Nxbdjekkwso.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
+echo "Please input link to your vps data backup file."
+echo "You can check it on your email if you run backup data vps before."
+read -rp "Link File: " -e url
+wget -O backup.zip "$url"
+unzip backup.zip
+rm -f backup.zip
+sleep 1
+echo Start Restore
+cd /root/backup
+cp -r xray /etc/
+cp passwd /etc/
+cp group /etc/
+cp gshadow /etc/
+cp config.json /usr/local/etc/xray/
+strt
+rm -rf /root/backup
+rm -f backup.zip
+echo Done
+
