edd81ee668ec5723ac895aea6578e5674b40931c
scriptvpskita <88125458+scriptvpskita@users.noreply.github.com>
2022-01-25 12:55:30 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,61 +4,40 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/xkjdox/rijrekeksdpdolwoqkqkakodix/main/Nxbdjekkwso.txt | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
-clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
-	fi
-
-	clear
-	echo ""
-	echo " Select the existing client you want to monitoring"
-	echo ""
-	echo " ==============================="
-	echo " User  Expired"
-	grep -E "^### " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3
-echo ""
 echo -n > /tmp/other.txt
-read -rp "username: " -e akun
-clear
-echo "----------------------------------";
-echo "-----=[ Monitor User Login ]=-----";
-echo "----------------------------------";
-echo -n > /tmp/ipvmess.txt
+data=( `cat /usr/local/etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
+echo "-------------------------------------";
+echo "-----=[ Monitoring User Login ]=-----";
+echo "-------------------------------------";
+for akun in "${data[@]}"
+do
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+echo -n > /tmp/ipxray.txt
 data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
 jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/ipvmess.txt
+echo "$jum" >> /tmp/ipxray.txt
 else
 echo "$ip" >> /tmp/other.txt
 fi
-jum2=$(cat /tmp/ipvmess.txt)
+jum2=$(cat /tmp/ipxray.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-jum=$(cat /tmp/ipvmess.txt)
+jum=$(cat /tmp/ipxray.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/ipvmess.txt | nl)
+jum2=$(cat /tmp/ipxray.txt | nl)
 echo "user : $akun";
 echo "$jum2";
+echo "-------------------------------------"
 fi
 rm -rf /tmp/ipvmess.txt
-echo "----------------------------------"
+done
 rm -rf /tmp/other.txt
-
-
 
 
