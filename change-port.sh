b5c56357408ac0d7b0daced6917659562a566978
scriptvpskita <88125458+scriptvpskita@users.noreply.github.com>
2022-01-27 18:59:00 +0800

    Create change-port.sh

---
+++ change-port.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/xkjdox/rijrekeksdpdolwoqkqkakodix/main/Nxbdjekkwso.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+ssl="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $1}')"
+tls="$(cat ~/log-install.txt | grep -w "AIO" | cut -d: -f2|sed 's/ //g')"
+echo -e "======================================"
+echo -e ""
+echo -e "   [1]  Change Port SSH WS SSL  $ssl"
+echo -e "   [2]  Change Port Vmess,Vless,Trojan  $tls"
+echo -e "   [x]  Exit"
+echo -e "======================================"
+echo -e ""
+read -p "     Select From Options [1-2 or x] :  " prot
+echo -e ""
+case $prot in
+1)
+read -p "New Port SSH WS SSL : " stl
+if [ -z $stl ]; then
+echo "Please Input Port"
+exit 0
+fi
+cek=$(netstat -nutlp | grep -w $stl)
+if [[ -z $cek ]]; then
+sed -i "s/$ssl/$stl/g" /etc/stunnel/stunnel.conf
+sed -i "s/   - Stunnel4                : $ssl/   - Stunnel4                : $stl/g" /root/log-install.txt
+/etc/init.d/stunnel4 restart > /dev/null
+systemctl restart cdn > /dev/null
+echo -e "\e[032;1mPort $stl modified successfully\e[0m"
+else
+echo "Port $stl is used"
+fi
+;;
+2)
+read -p "New Port Vmess,Vless,Trojan: " tls1
+if [ -z $tls1 ]; then
+echo "Please Input Port"
+exit 0
+fi
+cek=$(netstat -nutlp | grep -w $tls1)
+if [[ -z $cek ]]; then
+sed -i "s/$tls/$tls1/g" /usr/local/etc/xray/config.json
+sed -i "s/   - XRAY AIO                : $tls/   - XRAY AIO                : $tls1/g" /root/log-install.txt
+chmod -R 755 /etc/ssl/private/
+iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $tls -j ACCEPT
+iptables -D INPUT -m state --state NEW -m udp -p udp --dport $tls -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $tls1 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport $tls1 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save > /dev/null
+netfilter-persistent reload > /dev/null
+systemctl restart xray > /dev/null
+echo -e "\e[032;1mPort $tls1 modified successfully\e[0m"
+else
+echo "Port $tls1 is used"
+fi
+;;
+x)
+exit
+menu
+;;
+*)
+echo "Please enter an correct number"
+;;
+esac
+
+
